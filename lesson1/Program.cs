8f952f24c7e806c430f79731f53d9256cef4a0c6
PC <PC@DESKTOP-3OU8KQ7>
2024-10-14 13:54:31 +0300

    Add project files.

---
+++ lesson1/Program.cs
@@ -0,0 +1,7 @@
+﻿byte innteger = 255; // Represent an 8-bit unsigned integer from 0 to 255.
+bool check = false; // Represents a Boolean value, which can be "true" or "false".
+char letter = '\u004B'; // Represents a character as a UTF-16 code unit.
+float value = 1.223445253f; // Represents a floating-point number with a precision of 6-9 digits.
+double variable = 4.255252535235235; // Represents a floating-point number with a precision of 15-17 digits.
+decimal dec = 5.3526215234532412341513452345m; // Represents a floating-point number with a precision of 28-29 digits.
+int number = 43; // Represents a 32-bit signed integer from -2,147,483,648 to 2,147,483,647.
