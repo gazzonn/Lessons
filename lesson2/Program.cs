7fa2f8467281e7c8ed3e0d9add0f8faa8d825be0
gazzonn <demondarklite@gmail.com>
2024-10-19 21:47:48 +0300

    Added lesson2 with "Guess the Number" game.

---
+++ lesson2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\"Guess the Number\" by gazzonn\nI thought of a number, try to guess it!");
+            int randomNumber = GenerateNumber();
+            while (true)
+            {
+                try
+                {
+                    int guessedNumber = Convert.ToInt16(Console.ReadLine());
+                    if (guessedNumber == randomNumber)
+                    {
+                        Console.WriteLine($"Yes! It's {randomNumber}. You won!");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("No. Try again!");
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("The input must be integer.");
+                }
+            }
+            
+        }
+        static int GenerateNumber()
+        {
+            Random rnd = new Random();
+            return rnd.Next(1, 11);
+        }
+    }
+}
